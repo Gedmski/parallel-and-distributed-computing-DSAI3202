48b233805f740f3ef7611c222ab74fba5dd8d899
Gabriel Edward Pabilona Marquez <gedmarquez04@gmail.com>
2025-03-16 10:52:04 +0300

    Modified main.py

--- main.py
+++ main.py
@@ -1,19 +1,33 @@
 from mpi4py import MPI
+import numpy as np
+from src.square import square
+import time
+import random
 
 comm = MPI.COMM_WORLD
 rank = comm.Get_rank()
+size = comm.Get_size()
+
+results = None
+
 if rank == 0:
-    # Process 0 sends a non-blocking message to Process 1
-    data_to_send = "Hello from Process 0"
-    request = comm.bcast(data_to_send, dest=1, tag=100)
-    # Process 0 can perform other work here while the send operation completes
-    request.wait() # Wait for the non-blocking send to complete
-    print("Process 0 sent data")
-elif rank > 0:
-    # Process 1 sets up a non-blocking receive from Process 0
-    request = comm.irecv(source=0, tag=100)
-    data_received = request.wait() # Wait for the non-blocking receive to complete
-    # 'status' object contains information about the received message
-    status = request.Get_status()
-    print(f"Process {rank} received data: {data_received}")
-    print("Status of the received message: ", status)
+    numbers = np.arange(size, dtype="i")
+else:
+    numbers = None
+
+number = np.zeros(1, dtype="i")
+comm.Scatter(numbers, number, root=0)
+
+result = square(number[0])
+
+time.sleep(random.randint(1,11))
+request = comm.isend(result, dest=0, tag=rank)
+print(result)
+
+if rank == 0:
+    results = np.zeros(size, dtype="i")
+    for i in range(size):
+        results[i] = comm.irecv(source=i, tag=i).wait()
+    print(f"The results are: {results}")
+    
+request.wait()
